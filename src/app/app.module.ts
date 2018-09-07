9bdba832f3386663c302848b473364a32e1b26d5
Zachary E Buzaid <zbuzaid@gmail.com>
2018-09-06 19:47:09 -0700

    files that didnt make the comit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,16 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { CarouselModule } from './core/components/carousel/carousel.module';
 import { CommonModule } from '@angular/common';
+import { IncomeComponent } from './core/components/income/income.component';
+import { AnimationComponent } from './core/components/animation/animation.component';
 
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    IncomeComponent,
+    AnimationComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/core/services/income.service.ts
@@ -0,0 +1,15 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class IncomeService {
+
+  constructor() { }
+
+  /**
+   * Returns the income information associated with a user
+   */
+  getUsersIncome() {
+  }
+}
