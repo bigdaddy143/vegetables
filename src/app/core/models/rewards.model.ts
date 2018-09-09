e014fd7d52480a29de55a1ea5b280407b5f3f2f6
Zachary E Buzaid <zbuzaid@gmail.com>
2018-09-08 21:16:38 -0700

    styling for days

--- src/app/core/models/rewards.model.ts
+++ src/app/core/models/rewards.model.ts
@@ -5,7 +5,6 @@ export class Reward {
     RewardDescription: string;
     RewardXP: number;
     RewardImage: string;
+    RemainingTime: number;
     Selected: boolean; // need to determine if the backend is storing selected state
 }
-
-
